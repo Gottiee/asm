38e7226a04a0468a737d13628b8b29a875def4c7
Gottiee <gottiedev@gmail.com>
2023-05-21 14:54:16 +0200

    create strcpy

---
+++ src/ft_strcpy.s
@@ -0,0 +1,7 @@
+global _ft_strcpy
+
+section .text
+
+_ft_strcpy:
+    
+; sous l'architecture x86-64, les six premiers arguments sont généralement passés dans les registres rdi, rsi, rdx, rcx, r8 et r9, respectivement.
